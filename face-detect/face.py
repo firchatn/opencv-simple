45f86e1b87a35ef4274c86e5e7184f9291320f97
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-19 21:53:15 +0100

    update

--- face-detect/face.py
+++ face-detect/face.py
@@ -14,14 +14,18 @@ while 1:
 
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+        
+        #cv2.circle(img,(x,y),(x+w,y+h), (0,0,255), -1)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-    cv2.imshow('img',img)
+   
+    cv2.imshow('live video',img)
+    
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
