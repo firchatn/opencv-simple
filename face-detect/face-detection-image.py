8fac4d8f41270342de17d0c9df20facc359d29ae
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-20 19:35:26 +0100

    create cam graphiquel inteface folder

--- face-detect/face-detection-image.py
+++ face-detect/face-detection-image.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('img3.jpg')
+img = cv2.imread('images/img3.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
