46d30aa8cd8a305cd2128db4abbd55941d1d04af
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-20 17:52:32 +0100

    update script multi face

--- face-detect/face-detection-image.py
+++ face-detect/face-detection-image.py
@@ -2,17 +2,19 @@ import numpy as np
 import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('imgtest.jpg')
+img = cv2.imread('img3.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 only_face = np.array(10)
+nb = 0 
 for (x,y,w,h) in faces:
+    nb +=1
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    only_face = gray[y:y+h,x:x+w]
+    only_face = img[y:y+h,x:x+w]
+
+    cv2.imshow('face'+str(nb), only_face)
     
-    
-cv2.imshow('face', only_face)  
 cv2.imshow('img',img)
 cv2.imwrite("data/user"+".jpg", only_face)
 cv2.waitKey(0)
