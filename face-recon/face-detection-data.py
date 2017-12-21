6951ee336465780a06d6b3376fa51af06bde3391
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-21 15:41:01 +0100

    support data store

--- face-recon/face-detection-data.py
+++ face-recon/face-detection-data.py
@@ -6,7 +6,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 
 def write_data():
-    id= raw_input('put id')
+    id= int(input('put id'))
     nb = 0 
     while 1:
         ret, img = cap.read()
@@ -16,10 +16,10 @@ def write_data():
         for (x,y,w,h) in faces:
             nb +=1
             if nb ==1:
-                os.system('mkdir user{0}'.format(id))
+                os.system('mkdir data/user{0}'.format(id))
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
             only_face = gray[y:y+h,x:x+w]
-            cv2.imwrite("data/user."+str(id)+".jpg", only_face)
+            cv2.imwrite("data/user"+str(id)+"/"+str(nb)+".jpg", only_face)
         cv2.imshow('live video',img)
         
         cv2.waitKey(100)
