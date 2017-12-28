5d9aa35f1a63345a992ad10d294922bebf7375bc
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-28 21:12:43 +0100

    delete invalid function

--- cam-gi-gtk/interface.py
+++ cam-gi-gtk/interface.py
@@ -6,21 +6,14 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GLib, GdkPixbuf
  
 cap = cv2.VideoCapture(0)
- 
 builder = Gtk.Builder()
 builder.add_from_file("layout.glade")
- 
-greyscale = False
- 
+
 class Handler:
     def onDeleteWindow(self, *args):
         cap.release
         Gtk.main_quit(*args)
- 
-    def toggleGreyscale(self, *args):
-        global greyscale
-        greyscale = ~ greyscale
- 
+
 window = builder.get_object("window1")
 image = builder.get_object("image1")
 window.show_all()
@@ -29,14 +22,13 @@ builder.connect_signals(Handler())
 def show_frame(*args):
     ret, frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-     
     pb = GdkPixbuf.Pixbuf.new_from_data(frame.tostring(),
                                         GdkPixbuf.Colorspace.RGB,
                                         False,
                                         8,
                                         frame.shape[1],
                                         frame.shape[0],
-                                        frame.shape[2]*frame.shape[1])
+                                        frame.shape[2]*frame.shape[1])                            
     image.set_from_pixbuf(pb.copy())
     return True
  
