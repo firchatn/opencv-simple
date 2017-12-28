5b0bdf19284f73d256b4686ad845caefca670cf0
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-28 23:52:22 +0100

    update face recon exemple

--- face-recon/face-detection-data.py
+++ face-recon/face-detection-data.py
@@ -2,8 +2,10 @@ import numpy as np
 import cv2
 import os
 import os.path
+from PIL import Image
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_recognizer = cv2.face.createLBPHFaceRecognizer()
 cap = cv2.VideoCapture(0)
 
 def write_data():
@@ -30,23 +32,44 @@ def write_data():
             cv2.destroyAllWindows()
 
 def train_data():
-    faces = []
-    labels =[ 1,2,3 ]
     
+    images = []
+    labels =[]
     dirs = os.listdir('./data')
+    
     for dir in dirs:
-        face = np.array(10)
         for i in range(20):
             face = cv2.imread('data/{0}/{1}.jpg'.format(dir,i+1))
-            print(face)
-            #cv2.imshow('face'+str(i),face)
-        faces.append(face)
-    
-    face_recognizer = cv2.face.createLBPHFaceRecognizer()
-    face_recognizer.train(faces, np.array(labels))
-    
+            image_pil = Image.open('data/{0}/{1}.jpg'.format(dir,i+1)).convert('L')
+            image = np.array(image_pil, 'uint8')
+            nbr = int(dir[-1])
+            print(nbr)
+            faces = face_cascade.detectMultiScale(image)
+            for (x, y, w, h) in faces:
+                images.append(image[y: y + h, x: x + w])
+                labels.append(nbr)
+                cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
+                cv2.waitKey(10)
 
+    face_recognizer.train(images, np.array(labels))
+    face_recognizer.save('trainer/trainer.yml')
+    cv2.destroyAllWindows()
+            
 
 def recon_data():
-    pass
+    while True:
+        ret, im =cap.read()
+        gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        faces=face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+        for(x,y,w,h) in faces:
+            id_user, conf = face_recognizer.predict(gray[y:y+h,x:x+w])
+            cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
+            if(id_user==1):
+                 id_user='Firas'
+            elif(id_user==2):
+                 id_user='Moez'
+            
+            cv2.putText(im,str(id_user), (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+            cv2.imshow('im',im)
+            cv2.waitKey(10)
 
