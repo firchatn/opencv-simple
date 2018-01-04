3b01deb3e0a72d19488894617d9a4033360daaf9
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-04 22:15:36 +0100

    update storage

--- cam-gi-gtk/interface.py
+++ cam-gi-gtk/interface.py
@@ -4,7 +4,10 @@ import gi
  
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GLib, GdkPixbuf
- 
+
+imgf = cv2.imread('icon/record.png')
+resf = cv2.resize(imgf,None,fx=0.2, fy=0.2, interpolation = cv2.INTER_CUBIC)
+
 cap = cv2.VideoCapture(0)
 builder = Gtk.Builder()
 builder.add_from_file("layout.glade")
@@ -12,7 +15,7 @@ builder.add_from_file("layout.glade")
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'),
+out = cv2.VideoWriter('video/outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'),
                       10, (frame_width,frame_height))
 record_on = False
 
@@ -39,8 +42,19 @@ new_action.connect("activate", record)
     
 def show_frame(*args):
     ret, frame = cap.read()
+    img = frame
     if record_on :
         out.write(frame)
+        rows,cols,channels = resf.shape
+        rows1,cols1,channels1 = img.shape
+        roi = img[0:rows, 0:cols ]
+        img2gray = cv2.cvtColor(resf,cv2.COLOR_BGR2GRAY)
+        ret, mask = cv2.threshold(img2gray, 0, 255, cv2.THRESH_BINARY)
+        mask_inv = cv2.bitwise_not(mask)
+        img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+        img2_fg = cv2.bitwise_and(resf,resf,mask = mask)
+        dst = cv2.add(img1_bg,img2_fg)
+        img[0:rows, 0:cols ] = dst
         
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     pb = GdkPixbuf.Pixbuf.new_from_data(frame.tostring(),
